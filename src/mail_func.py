1e3af3a602c5016630e00bc9238b75ebf53a77f7
Divyansh Choudhary <divyanshchoudhary99@gmail.com>
2019-04-05 00:19:24 +0530

    stuff

--- src/mail_func.py
+++ src/mail_func.py
@@ -13,14 +13,14 @@ def mail_func(form, file_name):
     email_user = 'id'
     email_password = 'password'
     email_send = form['recipient_email']
-    email_Cc = form['Cc']
+    # email_Cc = form['Cc']
     subject = form['subject']
 
     msg = MIMEMultipart()
     msg['From'] = email_user
     msg['To'] = ", ".join(email_send)
     msg['Subject'] = subject
-    msg['Cc'] = ", ".join(email_Cc)
+    # msg['Cc'] = ", ".join(email_Cc)
 
     body = 'Sent by ' + form['sender_name'] + '\n' + now.strftime("%Y-%m-%d %H:%M") + '\n' + form['body']
     msg.attach(MIMEText(body,'plain'))
@@ -39,5 +39,5 @@ def mail_func(form, file_name):
     server.login(email_user, email_password)
 
 
-    server.sendmail(email_user, email_send + email_Cc, text)
+    server.sendmail(email_user, email_send , text)
     server.quit()
--- src/portal.py
+++ src/portal.py
@@ -86,12 +86,24 @@ def account():
     return render_template('account.html', title='Account',current_user=current_user, data = getdata(current_user.user_id))
 
 @app.route("/upload",methods=['POST'])
+@login_required
 def upload():
     file = request.files['inputFile']            # add tag of file here
     newFile = Files(name=file.filename,data=file.read())
     db.session.add(newFile)
+    newFile.mapping.append(current_user)
     db.session.commit()
-    return "saved to database"
+    return render_template('account.html', title='Account',current_user=current_user, data = getdata(current_user.user_id))
+
+@app.route("/share")
+@login_required
+def share():
+    form = ShareForm()
+    if form.validate_on_submit():
+        for recipient in form['recipient_email']:
+            user = User.query.filter_by(email=recipient).first()
+            if user is not None:
+                
 
 @app.route("/download")
 def download():
@@ -103,6 +115,51 @@ def download():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+
+def mail_func(form, file_name):
+    import smtplib
+    import datetime
+
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.base import MIMEBase
+    from email import encoders
+
+
+    now = datetime.datetime.now()
+
+    email_user = 'id'
+    email_password = 'password'
+    email_send = form['recipient_email']
+    # email_Cc = form['Cc']
+    subject = form['subject']
+
+    msg = MIMEMultipart()
+    msg['From'] = email_user
+    msg['To'] = ", ".join(email_send)
+    msg['Subject'] = subject
+    # msg['Cc'] = ", ".join(email_Cc)
+
+    body = 'Sent by ' + form['sender_name'] + '\n' + now.strftime("%Y-%m-%d %H:%M") + '\n' + form['body']
+    msg.attach(MIMEText(body,'plain'))
+    
+    attachment  = open(file_name,'rb')
+
+    part = MIMEBase('application','octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition',"attachment; filename= " + file_name)
+
+    msg.attach(part)
+    text = msg.as_string()
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email_user, email_password)
+
+
+    server.sendmail(email_user, email_send , text)
+    server.quit()
 
 if __name__ == '__main__':
     app.run(debug=True)
